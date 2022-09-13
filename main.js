3fb085df4cace13bd56a2b93843fc4665fdf9287
Proxtx <39201896+Proxtx@users.noreply.github.com>
2022-09-13 09:30:17 +0200

    working

---
+++ main.js
@@ -0,0 +1,30 @@
+import { genModule } from "@proxtx/combine/combine.js";
+import { genCombine } from "@proxtx/combine-rest/request.js";
+import config from "@proxtx/config";
+
+const statsApi = await genCombine(
+  config.RedditDownloader.api + "/",
+  "public/stats.js",
+  genModule
+);
+
+export class Info {
+  async info() {
+    let stats = await statsApi.getAllStats(config.RedditDownloader.pwd);
+    let returnObj = [];
+    for (let name in stats) {
+      returnObj.push({
+        element: "title-box-io",
+        title: name,
+        contains: {
+          element: "text-io",
+          size: "90%",
+          color: "var(--greyColor)",
+          text: stats[name],
+        },
+      });
+    }
+
+    return returnObj;
+  }
+}
